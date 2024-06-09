cffb51c36f478e9012d7f183284de9a0a5cfad0d
Muhammad Fastabiqul Khairat <fastabiqulk515@gmail.com>
2024-06-09 22:41:55 +0700

    Change Password and Profile

--- app/src/main/java/com/fasta/stuntguard/profile/edit/EditProfileActivity.kt
+++ app/src/main/java/com/fasta/stuntguard/profile/edit/EditProfileActivity.kt
@@ -27,47 +27,28 @@ class EditProfileActivity : AppCompatActivity() {
         actionbar?.title = "Edit Profile"
         actionbar?.setDisplayHomeAsUpEnabled(true)
 
-
-        // Load user data and populate UI
-        //loadUserData()
-
         binding.btnEditprofile.setOnClickListener {
             saveChanges()
         }
     }
 
-//    private fun loadUserData() {
-//        viewModel.getUserData().observe(this, { userData ->
-//            // Check if userData is not null
-//            userData?.let {
-//                // Populate UI with user data
-//                binding.nama.setText(it.parentName)
-//                binding.email.setText(it.email)
-//                binding.hp.setText(it.phone ?: "")
-//            }
-//        })
-//    }
-
     private fun saveChanges() {
 
         profileViewModel.getUserData().observe(this, { user ->
-            val parentId = user.userId // Assume parentId is a property of UserModel
+            val parentId = user.userId
             val parentName = binding.nama.text.toString().trim()
             val email = binding.email.text.toString().trim()
             val phone = binding.hp.text.toString().trim()
 
             profileViewModel.updateProfile(parentId, parentName, email, phone)
 
-            // Validate input
             if (parentName.isEmpty() || email.isEmpty()) {
                 Toast.makeText(this, "Parent name and email cannot be empty", Toast.LENGTH_SHORT).show()
                 return@observe
             }
 
-            // Update profile
             profileViewModel.updateProfile(parentId, parentName, email, phone)
 
-            // Show a toast message or handle UI feedback to indicate that changes are saved
             Toast.makeText(this, "Profile updated successfully", Toast.LENGTH_SHORT).show()
             finish()
         })
